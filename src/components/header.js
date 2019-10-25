310ed59c18ebd9000536d652480c0ca242c411af
Ignacio Bejarano <ignbejarano@gmail.com>
2019-10-25 18:14:55 -0300

    implemented a page functioning with static data

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'gatsby';
+//import './header.module.scss';
+import headerStyles from './header.module.scss'
+
 
 const Header = () => {
     return (
-        <header>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to='/'>My pagina web</Link>
+            </h1>
             <nav>
-                <ul>
-                    <li> <Link to='/'> Home </Link></li>
-                    <li> <Link to='/blog'> Blog </Link></li>
-                    <li> <Link to='/contact'> Contact </Link></li>
-                    <li> <Link to='/about'> About Me </Link></li>
+                <ul className={headerStyles.navList}>
+                    <li> <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'> Home </Link></li>
+                    <li> <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'> Blog </Link></li>
+                    <li> <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'> Contact </Link></li>
+                    <li> <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'> About Me </Link></li>
                 </ul>
             </nav>
         </header>
