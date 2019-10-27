d984f6f8afe2e94edf4f17544ee641be89b3f128
Ignacio Bejarano <ignbejarano@gmail.com>
2019-10-27 18:44:07 -0300

    added 404 custom page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import Head from '../components/head';
 
 const IndexPage = () => {
     return(
         <Layout>
+            <Head />
             <h1>Hello,</h1>
             <h2>I'm Ignacio, a full-stack developer living in Bs As, help!</h2>
             <p>Need a developer? <Link to='/contact'>Contact me!</Link></p>
