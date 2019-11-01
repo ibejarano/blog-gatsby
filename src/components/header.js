9b0b353587df341f73c52b1379ee59743f793db9
Ignacio Bejarano <ignbejarano@gmail.com>
2019-11-01 11:50:36 -0300

    changed header layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,90 @@
 import React from 'react';
-import { Link, graphql, useStaticQuery } from 'gatsby';
-//import './header.module.scss';
-import headerStyles from './header.module.scss'
+import { graphql, useStaticQuery } from 'gatsby';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-const Header = () => {
+const useStyles = makeStyles(theme => ({
+    toolbar: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbarTitle: {
+      flex: 1,
+    },
+    toolbarSecondary: {
+      justifyContent: 'space-between',
+      overflowX: 'auto',
+    },
+    toolbarLink: {
+      padding: theme.spacing(1),
+      flexShrink: 0,
+    },
+    mainFeaturedPost: {
+      position: 'relative',
+      backgroundColor: theme.palette.grey[800],
+      color: theme.palette.common.white,
+      marginBottom: theme.spacing(4),
+      backgroundImage: 'url(https://source.unsplash.com/user/erondu)',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+    },
+    overlay: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0,
+      backgroundColor: 'rgba(0,0,0,.3)',
+    },
+    mainFeaturedPostContent: {
+      position: 'relative',
+      padding: theme.spacing(3),
+      [theme.breakpoints.up('md')]: {
+        padding: theme.spacing(6),
+        paddingRight: 0,
+      },
+    },
+    mainGrid: {
+      marginTop: theme.spacing(3),
+    },
+    card: {
+      display: 'flex',
+    },
+    cardDetails: {
+      flex: 1,
+    },
+    cardMedia: {
+      width: 160,
+    },
+    markdown: {
+      ...theme.typography.body2,
+      padding: theme.spacing(3, 0),
+    },
+    sidebarAboutBox: {
+      padding: theme.spacing(2),
+      backgroundColor: theme.palette.grey[200],
+    },
+    sidebarSection: {
+      marginTop: theme.spacing(3),
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      marginTop: theme.spacing(8),
+      padding: theme.spacing(6, 0),
+    },
+  }));
 
+export default function Header() {
+    
+    const classes = useStyles();
+    
     const data = useStaticQuery(graphql`
         query {
             site {
@@ -16,21 +95,55 @@ const Header = () => {
         }
     `);
 
+    const sections = [
+        'Technology',
+        'Design',
+        'Culture',
+        'Business',
+        'Politics',
+        'Opinion',
+        'Science',
+        'Health',
+        'Style',
+        'Travel',
+      ];
+
     return (
-        <header className={headerStyles.header}>
-            <h1>
-                <Link className={headerStyles.title} to='/'>{data.site.siteMetadata.title}</Link>
-            </h1>
-            <nav>
-                <ul className={headerStyles.navList}>
-                    <li> <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'> Home </Link></li>
-                    <li> <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'> Blog </Link></li>
-                    <li> <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'> Contact </Link></li>
-                    <li> <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'> About Me </Link></li>
-                </ul>
-            </nav>
+        <header>
+            <CssBaseline />
+            <Toolbar className={classes.toolbar}>
+                <Button size="small">Subscribe</Button>
+                <Typography
+                    component="h2"
+                    variant="h5"
+                    color="inherit"
+                    align="center"
+                    noWrap
+                    className={classes.toolbarTitle}
+                >
+                    {data.site.siteMetadata.title}
+                </Typography>
+                <IconButton>
+                    <SearchIcon />
+                </IconButton>
+                <Button variant="outlined" size="small">
+                    Sign up
+                </Button>
+                </Toolbar>
+                <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
+                {sections.map(section => (
+                    <Link
+                    color="inherit"
+                    noWrap
+                    key={section}
+                    variant="body2"
+                    href="#"
+                    className={classes.toolbarLink}
+                    >
+                    {section}
+                    </Link>
+                ))}
+            </Toolbar>
         </header>
     )
 }
-
-export default Header;
