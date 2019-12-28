48752e7770c043751bee28e641a65dfde3a608b2
Ignacio Bejarano <ignbejarano@gmail.com>
2019-12-28 12:14:15 -0300

    added post images rendering

--- src/styles/blogBody.js
+++ src/styles/blogBody.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components'
 
 const BlogStyle = styled.div`
-    width: 50%;
-    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    h1 {
+        width: 75%;
+        margin-bottom: 0;
+    }
     .post-date {
         font-size: 1.1rem;
+        width: 75%;
+        margin: 0;
     }
     .post-body {
         font-size: 1.25rem;
+        width: 50%;
     }
-
+    .post-image {
+        width: 80%;
+    }
 
 `
 
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Layout from '../components/layout';
-import {graphql} from 'gatsby';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import Head from '../components/head';
-import BlogStyle from '../styles/blogBody';
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Head from "../components/head"
+import BlogStyle from "../styles/blogBody"
 
 // export const query = graphql`
 //     query (
@@ -25,43 +25,58 @@ import BlogStyle from '../styles/blogBody';
 //     }
 // `
 
-
 export const query = graphql`
-    query ($slug: String!) {
-        contentfulBlogPost(slug: {eq: $slug}) {
-            title
-            publishedDate(formatString: "MMM Do, YYYY")
-            body {
-                json
-            }
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      postImage {
+        file {
+          url
         }
+      }
+      publishedDate(formatString: "MMM Do, YYYY")
+      body {
+        json
+      }
     }
+  }
 `
 
-const Blog = (props) => {
+const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["en-US"]
+        const url = node.data.target.fields.file["en-US"].url
+        return <img alt={alt} src={url} />
+      },
+    },
+  }
 
-    const options = {
-        renderNode: {
-            "embedded-asset-block": (node) => {
-                const alt = node.data.target.fields.title['en-US']
-                const url = node.data.target.fields.file['en-US'].url
-                return <img alt={alt} src={url} />
-            }
-        }
-    }
-
-    return(
-        <Layout>
-            <Head title={props.data.contentfulBlogPost.title} />
-           <BlogStyle>
-           <h1>{props.data.contentfulBlogPost.title}</h1>
-           <p className="post-date" >{props.data.contentfulBlogPost.publishedDate}</p>
-           <div className="post-body" >
-           {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
-           </div>
-           </BlogStyle>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Head title={props.data.contentfulBlogPost.title} />
+      <BlogStyle>
+        <h1>{props.data.contentfulBlogPost.title}</h1>
+        <p className="post-date">
+          {props.data.contentfulBlogPost.publishedDate}
+        </p>
+        {props.data.contentfulBlogPost.postImage && (
+          <img
+            className="post-image"
+            src={props.data.contentfulBlogPost.postImage.file.url}
+            alt="post main image"
+          />
+        )}
+        <div className="post-body">
+          {documentToReactComponents(
+            props.data.contentfulBlogPost.body.json,
+            options
+          )}
+        </div>
+      </BlogStyle>
+    </Layout>
+  )
 }
 
-export default Blog;
+export default Blog
